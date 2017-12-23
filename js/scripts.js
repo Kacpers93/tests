cbd79d747d3b4d194bb9ee788dd3e63c7edf0190
Kacper Wołoszyn <budzio161@gmail.com>
2017-12-23 18:27:41 +0100

    Change content

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,28 @@ console.log(multiplay(5, 2));
 console.log(multiplay(6, -2));
 
 console.log(multiplay(-5, -2));
+
+
+
+/*
+ROZWIĄZANIE MENTORA (do głąbszej analizy)
+function multiply(a, b) {
+    var aIsNegative = a < 0 && b > 0; 
+    var bIsNegative = a > 0 && b < 0; 
+    var isResultNegative = aIsNegative || bIsNegative;
+
+    a = Math.abs(a);
+    b = Math.abs(b);
+    var result = 0;
+    for (var i = 0; i < a; i++) {
+        result += b;
+    }
+
+    return isResultNegative ? -result : result;
+}
+
+console.log(multiply(3, 7));
+console.log(multiply(-3, 7));
+console.log(multiply(3, -7));
+console.log(multiply(-3, -7));
+*/
