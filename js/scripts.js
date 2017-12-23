9f37161e1fccbea2e57e763b41f38c61df4d9094
Kacper Wołoszyn <budzio161@gmail.com>
2017-12-23 14:10:22 +0100

    Change content

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,21 @@
 function multiplay(a, b) {
+    var result = 0;
+    if(b < 0 && a < 0) {
+        for (var i = 0; i < (-b); i++) {
+            result += (-a);
+        }
+    }
     if(b > 0) {
-        var result = 0;
         for (var i = 0; i < b; i++) {
             result += a;
         }
     } 
-    else if(b < 0) {
-        var result = 0;
+    if(b < 0) {
         for (var i = 0; i < a; i++) {
             result += b;
         }
     }
+    
     return result;
 }
 
@@ -19,3 +24,5 @@ console.log(multiplay(6, 2));
 console.log(multiplay(-6, 2));
 console.log(multiplay(5, 2));
 console.log(multiplay(6, -2));
+
+console.log(multiplay(-5, -2));
